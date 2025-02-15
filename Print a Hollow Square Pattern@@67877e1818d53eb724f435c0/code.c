7fe39bda8d37626bf327a99af8c508d32dfba35e
jash-28 <jashaggarwal28@gmail.com>
2025-02-15 21:34:55 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -2,8 +2,26 @@
 #include <stdio.h>
 int main()
 {
-    int N;
+    int N,space;
+    space=N-2;
     scanf("%d",&N);
+    for (int i=0; i<N;i++){
+        for (int j=0: j<N;j++){
+            if (i==j){
+                for(k=0 ;k<n;k++);{
+                    printf("* ");
+            else{
+                printf("* "){
+                    for (int l; l<space ; l++){
+                        print(" ")
+                    }
+                print("*")
+                }
+            }
+                }
+            }
+        }
+    }
     
 
     return 0;
