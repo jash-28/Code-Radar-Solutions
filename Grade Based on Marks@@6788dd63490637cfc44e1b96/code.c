ebac2e0b1d6f0784f959ffcf4909e2ac5144d499
jash-28 <jashaggarwal28@gmail.com>
2025-02-21 18:38:37 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+    int g;
+    scanf("%d",&g);
+    if (g>=90)
+    {
+        printf("A");
+    }
+    if else(g>=80 && g<90)
+    {
+        printf("B");
+    }
+    if else(g>=70 && g<80)
+    {
+        printf("C");
+    }
+    if else(g>=60 && g<70)
+    {
+        printf("D");
+    }
+    else
+    {
+        printf("F");
+    }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
