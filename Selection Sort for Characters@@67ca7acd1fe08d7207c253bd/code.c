933ff249cdd7a275bb05b7dfdf3eed6aaecd792f
jash-28 <jashaggarwal28@gmail.com>
2025-03-30 13:06:57 +0530

    Solution to Selection Sort for Characters

--- Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+def selection_sort_chars(n, chars):
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if chars[j] < chars[min_index]:
+                min_index = j
+        chars[i], chars[min_index] = chars[min_index], chars[i]
+    return ' '.join(chars)
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
+# Input
+n = int(input())
+chars = input().split()
 
-int main() {
-    printf("%s", welcome());
-    return 0;
-}
+# Sorting using selection sort
+result = selection_sort_chars(n, chars)
+print(result)
